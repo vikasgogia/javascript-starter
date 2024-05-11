83b90eef4f695c6feefb1142ad20c81422106d2c
vikasgogia <vikasgog10@gmail.com>
2024-05-11 15:17:05 -0400

    prototypal inheritance

--- oops/script.js
+++ oops/script.js
@@ -31,3 +31,56 @@ console.log(Person.prototype.isPrototypeOf(Person))
 Person.prototype.species = "Homo Sapiens"
 console.log(me.species)
 console.log(me.hasOwnProperty('fName'), me.hasOwnProperty('species'))
+
+
+
+
+// me -> Person.prototype -> Object.prototype -> null
+console.log(me.__proto__.__proto__.__proto__)
+
+// points back to Person
+console.dir(Person.prototype.constructor)
+
+// all the array methods like map(), indexOf()... lie in the Array.prototype
+// when we use [] for initializing an array, it's same as doing new Array()
+const arr = [1, 2, 3, 1]
+console.log(arr.__proto__)
+
+// a new array functionality can be added to Array.prototype
+Array.prototype.uniqueNums = function() {
+    return [...new Set(this)]
+}
+
+console.log(arr.uniqueNums())
+
+
+
+
+// coding challenge
+const Car = function(speed, make) {
+    this.speed = speed
+    this.make  = make
+}
+
+Car.prototype.accelerate = function() {
+    this.speed += 10
+    console.log("Accelerated: ", this.speed, " kms/hr")
+}
+
+Car.prototype.brake = function() {
+    this.speed -= 5
+    console.log("Applied brakes: ", this.speed, " kms/hr")
+}
+
+const bmw = new Car(120, 'bmw')
+const mer = new Car(95, 'mercedes')
+
+bmw.accelerate()
+bmw.brake()
+bmw.brake()
+bmw.accelerate()
+
+mer.accelerate()
+mer.brake()
+mer.brake()
+mer.accelerate()
